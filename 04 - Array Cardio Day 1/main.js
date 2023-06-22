20b5f3e1ebe063f306196085a5599210ca3f0949
gsngdavid <gsngdavid@gmail.com>
2023-06-22 16:02:54 +0200

    feat: Sum up the instances of each of item in array

--- 04 - Array Cardio Day 1/main.js
+++ 04 - Array Cardio Day 1/main.js
@@ -62,3 +62,10 @@
       // 8. Reduce Exercise
       // Sum up the instances of each of these
       const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+      let transportation = data.reduce((obj, item) => {
+        return {
+            ...obj,
+            [item]: (obj[item] ?? 0) + 1
+        }
+      }, {});
+      console.log(transportation);
