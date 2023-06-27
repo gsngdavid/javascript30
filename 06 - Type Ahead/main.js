49e2a3f5b7fae136db84537f9ad767ea30bb706d
gsngdavid <gsngdavid@gmail.com>
2023-06-27 10:14:09 +0200

    feat: Fetch and filter cities array

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,1 +1,31 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const input = document.querySelector('input');
+
+const cities = [];
+
+async function getData() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data;
+}
+
+function searchCity(word, cities) {
+    let patten = new RegExp(word, 'gi');
+    return cities.filter(city => {
+        return city.city.match(patten) || city.state.match(patten);
+    });
+};
+
+function handleInputChange(e) {
+    let value = e.currentTarget.value;
+    let filteredCities = searchCity(value, cities);
+    console.log(filteredCities);
+}
+
+
+getData().then(data => cities.push(...data)).catch(err => {
+    console.log(err);
+});
+
+input.addEventListener('input', handleInputChange);
