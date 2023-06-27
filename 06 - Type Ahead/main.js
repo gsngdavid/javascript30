dcfc116076748b2fc6adf1028cdb3b1594609f05
gsngdavid <gsngdavid@gmail.com>
2023-06-27 11:38:45 +0200

    feat: Display and highlight filtered places

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,6 +1,7 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const input = document.querySelector('input');
+const input = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
 const cities = [];
 
@@ -19,8 +20,20 @@ function searchCity(word, cities) {
 
 function handleInputChange(e) {
     let value = e.currentTarget.value;
-    let filteredCities = searchCity(value, cities);
-    console.log(filteredCities);
+    let filteredPlaces = searchCity(value, cities);
+
+    let html = filteredPlaces.map(place => {
+        let patten = new RegExp(value, 'gi');
+        let name = `${place.city}, ${place.state}`.replace(patten, `<span class="hl">${value}</span>`);
+        let population = Number(place.population).toLocaleString();
+
+        return `<li>
+            <span>${name}</span>
+            <span class='population'>${population}</span>
+        </li>`
+    }).join('');
+
+    suggestions.innerHTML = html;
 }
 
 
