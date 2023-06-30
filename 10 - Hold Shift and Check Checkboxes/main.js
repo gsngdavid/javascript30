30c31d607c4414d6c5efa51aa644c8b930491e87
gsngdavid <gsngdavid@gmail.com>
2023-06-30 11:15:06 +0200

    fix: Check for shift key press at the top

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -2,14 +2,14 @@ const inputs = [...document.querySelectorAll('input')];
 let lastCheckedIndex = null;
 
 const handleInputChange = e => {
-    if (!e.target.checked) {
+    if (!e.target.checked && !e.shiftKey) {
         lastCheckedIndex = null;
         return;
     }
 
     let checkedIndex = inputs.findIndex((input) => input === e.target);
     
-    if(lastCheckedIndex !== null && e.shiftKey) {
+    if(lastCheckedIndex !== null) {
         const [ minIndex, maxIndex ] = checkedIndex > lastCheckedIndex ? [ lastCheckedIndex, checkedIndex ] : [ checkedIndex, lastCheckedIndex ];
 
         for(let i = minIndex; i <= maxIndex; i++) {
