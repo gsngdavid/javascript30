1f57f1b0d8eaf23fe6b5fab070f36036750b89e5
gsngdavid <gsngdavid@gmail.com>
2023-07-15 14:03:04 +0200

    refactor: remove domContentLoaded

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,28 +1,26 @@
-document.addEventListener('DOMContentLoaded', e => {
-    const player = document.querySelector('.player');
-    const video = document.querySelector('video');
-    const toggle = document.querySelector(".toggle");
-    const progressBar = document.querySelector('.progress__filled');
+const player = document.querySelector('.player');
+const video = document.querySelector('video');
+const toggle = document.querySelector(".toggle");
+const progressBar = document.querySelector('.progress__filled');
 
 
 
-    function toggleHandler() {
-        if(video.paused) {
-            video.play();
-        }
-        else {
-            video.pause();
-        }
+function toggleHandler() {
+    if(video.paused) {
+        video.play();
     }
-
-    function updateBtn() {
-        const icon = this.paused ? '►' : '❚ ❚';
-        toggle.textContent = icon;
+    else {
+        video.pause();
     }
+}
+
+function updateBtn() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
 
 
 
-    player.addEventListener('click', toggleHandler);
-    video.addEventListener('play', updateBtn);
-    video.addEventListener('pause', updateBtn);
-});
+player.addEventListener('click', toggleHandler);
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
